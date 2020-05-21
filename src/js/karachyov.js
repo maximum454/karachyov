7c0422ff6e2b67161dce8e602da00823d3e713be
maxim <maximum454@gmail.com>
2020-05-21 12:49:49 +0600

    footer

--- src/js/karachyov.js
+++ src/js/karachyov.js
@@ -4,39 +4,40 @@
 @@include('partials/jquery.knob.js')
 @@include('partials/slick.js')
 
-$(document).scroll(function (event) {
-    var scroll = $(window).scrollTop(),
-        about = $('.about'),
-        aboutTop = about.offset().top,
-        aboutBottom = aboutTop + about.outerHeight(),
-        contacts = $('.contacts'),
-        contactsTop = contacts.offset().top,
-        contactsBottom = contactsTop + contacts.outerHeight();
-
-    if(scroll >= aboutTop && scroll < aboutBottom || scroll >= contactsTop && scroll < contactsBottom){
-        $('.header__burger, .header__social').addClass('color');
-    }else{
-        $('.header__burger, .header__social').removeClass('color');
-    }
+$(window).scroll(function(){
+    var $sections = $('section');
+    $sections.each(function(i,el){
+        var top  = $(el).offset().top-100;
+        var bottom = top +$(el).height();
+        var scroll = $(window).scrollTop();
+        var id = $(el).attr('id');
+        if( scroll > top && scroll < bottom){
+            $('a.active').removeClass('active');
+            $('a[href="#'+id+'"]').addClass('active');
+            if(id == 'about' || id == 'contacts'){
+                $('.header__burger, .header__social').addClass('color');
+            }else{
+                $('.header__burger, .header__social').removeClass('color');
+            }
+        }
+    })
 });
-
 $(".menuscroll").on("click","a", function (event) {
     event.preventDefault();
     var id  = $(this).attr('href'),
         top = $(id).offset().top;
-    $('.menuscroll a').removeClass('active');
-    $(this).addClass('active');
-    $('.header, .header__burger, .header__menu, .header__logo').removeClass('active');
     $('body,html').animate({scrollTop: top}, 1500);
 });
 
 $(".dial").knob({
     readOnly:true,
-    width: 160,
-    height: 160,
+    width: 100,
+    height: 100,
     fgColor: '#01B29C',
     inputColor: '#01B29C',
-    bgColor: '#0073B4'
+    bgColor: '#0073B4',
+    font: 'Montserrat',
+    fontSize: '18px'
 });
 
 $('.project-slider').slick({
